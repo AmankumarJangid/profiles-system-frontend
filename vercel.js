714fe3b0b80514663bd2f40efe289c0edfab475e
AmanKumarJangid <amanjangid7847@gmail.com>
2026-04-22 21:48:35 +0530

    Vercel json fix 2

--- vercel.js
+++ vercel.js
@@ -1,15 +1,12 @@
-export const config = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        // This will pull from your Vercel Environment Variables
-        destination: `${process.env.VITE_API_TARGET}/api/:path*`,
-      },
-      {
-        source: "/(.*)",
-        destination: "/index.html",
-      },
-    ];
-  },
-};
+{
+  "rewrites": [
+    {
+      "source": "/api/:path*",
+      "destination": "https://profile-system-backend-production.up.railway.app/"
+    },
+    {
+      "source": "/(.*)",
+      "destination": "/index.html"
+    }
+  ]
+}
